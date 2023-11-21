bfe2e2a9827af0522cc2d5c97a3dd2ce851a4bc3
akhil189709 <106308351+akhil189709@users.noreply.github.com>
2023-11-21 11:16:57 +0000

    variables done

---
+++ 01_basics/variables.js
@@ -0,0 +1,16 @@
+const accountId = 123223;
+let accountEmail = "akhil@google.com";
+var accountPassword = "akhil@123";
+
+ accountCity = "jaipur";
+
+ accountEmail = "ahjhf";
+ accountPassword = "123456";
+ accountCity = "delhi";
+  var accounstate;
+
+//accountId = 4; not allowed
+
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accounstate]);
