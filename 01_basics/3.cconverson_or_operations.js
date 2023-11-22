8d777e97a4bb6b9b7689a09d7fafa96c42c274f7
akhil189709 <106308351+akhil189709@users.noreply.github.com>
2023-11-22 06:15:33 +0000

    conversions done

---
+++ 01_basics/3.cconverson_or_operations.js
@@ -0,0 +1,43 @@
+let score = null;
+
+console.log(typeof score);
+console.log(typeof(score));
+
+
+let valueInNumber  = Number(score);
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+/*********************************operations************************************ */
+
+let value = 3;
+let negvalue = -value;
+
+console.log(negvalue);
+
+/**************** */
+
+console.log(2+2);
+console.log(2*2);
+console.log(2**5);
+console.log(10%3);
+console.log(44/5);
+
+/************************ */
+
+console.log(true);
+console.log(+true);
+console.log(+"");
+
+/******************* */
+
+console.log(null == 0);
+console.log(null > 0);
+console.log(null >= 0);
+
+/****************** */
+
+console.log(undefined == 0 );
+console.log( undefined < 0);
+console.log(undefined >= 0);
+console.log(undefined <= 0);
